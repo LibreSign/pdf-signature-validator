72a0b2bc7fd7d4f22e709fd98f73e3b9fd46ef20
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-09-04 11:49:29 -0300

    refactor: rename unsigned document state
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/Model/DocumentModificationState.php
+++ src/Model/DocumentModificationState.php
@@ -10,7 +10,7 @@ namespace LibreSign\PdfSignatureValidator\Model;
 enum DocumentModificationState: string
 {
     case FULLY_COVERED = 'fully_covered';
-    case UNSIGNED_REVISION = 'unsigned_revision';
+    case UNSIGNED_CONTENT = 'unsigned_content';
     case TRAILING_DATA = 'trailing_data';
     case INVALID_BYTE_RANGE = 'invalid_byte_range';
 }
