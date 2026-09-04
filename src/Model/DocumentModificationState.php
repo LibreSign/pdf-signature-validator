5cc050a2fa7cde454f8293fb141bffa0bbb4c6b9
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-09-04 11:30:16 -0300

    feat: add document modification state
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ src/Model/DocumentModificationState.php
@@ -0,0 +1,16 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace LibreSign\PdfSignatureValidator\Model;
+
+enum DocumentModificationState: string
+{
+    case FULLY_COVERED = 'fully_covered';
+    case UNSIGNED_REVISION = 'unsigned_revision';
+    case TRAILING_DATA = 'trailing_data';
+    case INVALID_BYTE_RANGE = 'invalid_byte_range';
+}
