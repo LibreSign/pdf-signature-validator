5cb8f5db2a709f8f6aab87f37d828a61f62ec975
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-09-04 11:58:09 -0300

    feat: expose PDF signature offset
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/Model/SignatureMetadata.php
+++ src/Model/SignatureMetadata.php
@@ -17,6 +17,7 @@ final class SignatureMetadata
         public readonly ?array $range,
         public readonly ?string $signatureType,
         public readonly bool $coversEntireDocument,
+        public readonly ?int $signatureOffset = null,
         public readonly ?DocumentModificationState $documentModificationState = null,
     ) {
     }
